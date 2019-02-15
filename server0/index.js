d685f13b2b8b722a897cb1c7019ede9d7cc9e26d
i.isaev <i.isaev@pronet.ua>
2019-02-15 16:04:53 +0200

    shedule testing

--- server0/index.js
+++ server0/index.js
@@ -1,6 +1,8 @@
 const createSubscriber = require('pg-listen');
+const cron = require('node-schedule');
 const conString = 'tcp://postgres:postgres@localhost:5432/tmpsequelize';
 const subscriber = createSubscriber({ connectionString: conString });
+
 subscriber.connect();
 subscriber.listenTo("myEvent");
 subscriber.notifications.on("myEvent", (payload) => {
@@ -10,3 +12,9 @@ subscriber.notifications.on("myEvent", (payload) => {
 function check(payload) {
 	console.log('PAYLOAD: ', payload);
 }
+
+var rule = new cron.RecurrenceRule();
+rule.second = 30;
+cron.scheduleJob(rule, function(){
+	console.log(new Date(), 'The 30th second of the minute.');
+});
