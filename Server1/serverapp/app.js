918ce91d07f3e914d3a2ff69ed2ddd0e72ca80ea
i.isaev <i.isaev@pronet.ua>
2019-02-25 10:51:39 +0200

    f

--- Server1/serverapp/app.js
+++ Server1/serverapp/app.js
@@ -1,3 +1,25 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let fs = require("fs");
+let app = express();
+let jsonParser = bodyParser.json();
+
+app.use(express.static(__dirname + "/public"));
+// получение списка данных
+app.get("http://localhost/api/users", function(req, res){
+  let content = fs.readFileSync(__dirname + "/users.json", "utf8");
+  let users = JSON.parse(content);
+  res.send(users);
+});
+
+let port = 3001;
+
+app.listen(port, function(){
+  console.log("Сервер ожидает подключения на порту: ", port);
+});
+
+
+/*
 const http = require("http");
 const fs = require("fs");
 http.createServer(function(request, response){
@@ -33,4 +55,5 @@ http.createServer(function(request, response){
     //     // response.write(request.url.substr(1))
     //     response.end("Hello World!");
     // }
-}).listen(3000);
+}).listen(3001);
+*/
