7385127cc0958bb10a3d3da7b2b2128d9d0312da
i.isaev <i.isaev@pronet.ua>
2019-03-12 12:09:29 +0200

    f

--- server0/index.js
+++ server0/index.js
@@ -1,12 +1,12 @@
 const createSubscriber = require('pg-listen');
 const cron = require('node-schedule');
-const conString = 'tcp://postgres:postgres@localhost:5432/tmpsequelize';
+const conString = 'tcp://sky:passwOrd@localhost:5432/tmpsequelize';
 const subscriber = createSubscriber({ connectionString: conString });
 
 subscriber.connect();
 subscriber.listenTo("myEvent");
 subscriber.notifications.on("myEvent", (payload) => {
-	if(payload.key1 === 'new') check(payload);
+	if(payload.key2 === 'new') check(payload);
 });
 
 function check(payload) {
@@ -16,5 +16,5 @@ function check(payload) {
 var rule = new cron.RecurrenceRule();
 rule.second = 30;
 cron.scheduleJob(rule, function(){
-	console.log(new Date(), 'The 30th second of the minute.');
+	// console.log(new Date(), 'The 30th second of the minute.');
 });
